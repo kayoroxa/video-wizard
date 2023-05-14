fcfc8c640da7fa4355e1444ba80d79466c1fbb7e
Caio <kayoroxa@gmail.com>
2023-05-14 00:28:41 -0300

    .

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -7,6 +7,13 @@ interface Props {
   channel_id?: number
 }
 
+function calcularDiferencaEmDias(timestamp: number) {
+  const umDiaEmMilissegundos = 24 * 60 * 60 * 1000
+  const dataAtual = new Date().setHours(0, 0, 0, 0)
+  const dataDoTimestamp = new Date(timestamp).setHours(0, 0, 0, 0)
+  return Math.floor((dataAtual - dataDoTimestamp) / umDiaEmMilissegundos)
+}
+
 export default function VideoCard({
   url,
   title,
@@ -18,11 +25,15 @@ export default function VideoCard({
     <div className="shadow-2xl rounded-2xl overflow-hidden w-80">
       <img src={url} className="w-full" alt="" />
       <main className="p-3 bg-blue-600 h-full">
-        <h1>{title}</h1>
+        <h1 className="">{title}</h1>
         <footer className="flex gap-3">
-          <span>{views} views</span>
-          <span>{publishedAt}</span>
-          <span>{rate}</span>
+          <span className="bg-blue-400">{views} views</span>
+          <span className="bg-blue-400">
+            {new Date(publishedAt).toLocaleDateString('pt-br')}
+          </span>
+          <span className="bg-blue-400">
+            {views / calcularDiferencaEmDias(publishedAt)} Views per day
+          </span>
         </footer>
       </main>
     </div>
